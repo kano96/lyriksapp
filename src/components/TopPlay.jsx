29920318fbb95c82f646344045310bf0dd5f679f
Kevin Torres <kevin.torres@kushkipagos.com>
2022-12-06 15:38:00 -0500

    add search bar functionality

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -93,7 +93,7 @@ const TopPlay = () => {
       </div>
       <div className="w-full flex flex-col mt-8 ">
         <div className="flex flex-row justify-between items-center">
-          <h2 className="text-white font-bold text-2xl">Top Artits</h2>
+          <h2 className="text-white font-bold text-2xl">Top Artists</h2>
           <Link to="/top-artists">
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
